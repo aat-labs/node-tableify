304e5ed77c48ca28fab25d92aa2c87642c5effff
Morlene Fisher <morlene@ashleylawrence.com>
2016-09-15 10:49:17 +0100

    Updated fork to produce markdown instead of tables

--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 node-tableify
 -------------
 
-From any JavaScript object, HTML tables create.
+From any JavaScript object, Markdown tables create.
 
 description
 -----------
 
 Given any Javascript object including Object, Array, Number, Date, null, undefined, primitives,
-etc, `tableify` generates HTML tables that represent each object. In the case of Arrays containing
+etc, `tableify` generates Markdown tables that represent each object. In the case of Arrays containing
 Objects (hash tables), it will generate a table with a header row containing the key names of the
 object found in the first element of the array.
 
@@ -30,7 +30,7 @@ usage
 ```javascript
 var tableify = require('tableify');
 
-var html = tableify({
+var md = tableify({
 	someArrayOfObjects : [
 		{ a : 1, b : 2, c : 3  }
 		, { a : 2, b : 3, c : 4 }
@@ -52,24 +52,23 @@ var html = tableify({
 	}
 });
 
-console.log(html);
+console.log(md);
 ```
 
 command line usage
 ------------------
 
-When installed with `npm install -g tableify` you get the tablify command line utility. 
-This is a simple command which accepts JSON from stdin and outputs html to stdout. I use
+When installed with `npm install -g tableify` you get the tablify command line utility.
+This is a simple command which accepts JSON from stdin and outputs md to stdout. I use
 it for emailing json files in a nice format for debugging purposes.
 
 The --style option prepends a &lt;style&gt; tag with some default css
 
 ```bash
-cat datafile.json | tableify --style | mutt -e 'set content_type="text/html"' me@myaddress.com
+cat datafile.json | tableify --style | mutt -e 'set content_type="text/md"' me@myaddress.com
 ```
 
 license
 -------
 
 MIT
-
--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function tableify(obj, columns, parents) {
 
     if (Array.isArray(obj)) {
         if (typeof obj[0] === 'object') {
-            buf.push('<table>','<thead>','<tr>');
+            buf.push('||');
 
             //loop through every object and get unique keys
             var keys = {};
@@ -35,49 +35,43 @@ function tableify(obj, columns, parents) {
             cols = Object.keys(keys);
 
             cols.forEach(function (key) {
-                buf.push('<th' + getClass(obj[0][key]) + '>', key, '</th>');
+                buf.push( key, '||');
             });
 
-            buf.push('</tr>', '</thead>', '<tbody>');
+            buf.push('\n');
 
             obj.forEach(function (record) {
-                buf.push('<tr>');
+                // buf.push('<tr>');
                 buf.push(tableify(record, cols, parents));
-                buf.push('</tr>');
+                buf.push('| \n');
             });
-
-            buf.push('</tbody></table>');
         }
         else {
-            buf.push('<table>','<tbody>');
             cols = [];
-            
+
             obj.forEach(function (val, ix) {
                 cols.push(ix);
-                buf.push('<tr>', '<td' + getClass(val) + '>', tableify(val, cols, parents), '</td>', '</tr>');
+                buf.push('|', tableify(val, cols, parents), ' | \n');
             });
-            
-            buf.push('</tbody>','</table>');
         }
-        
+
     }
     else if (obj && typeof obj === 'object' && !Array.isArray(obj) && !(obj instanceof Date)) {
         if (!columns) {
-            buf.push('<table>');
+            buf.push('||');
 
             Object.keys(obj).forEach(function (key) {
-                buf.push('<tr>', '<th' + getClass(obj[key]) + '>', key, '</th>', '<td' + getClass(obj[key]) + '>', tableify(obj[key], false, parents), '</td>', '</tr>');
+                buf.push('' + getClass(obj[key]) + '>', key,  '|', tableify(obj[key], false, parents), ' | \n');
             });
 
-            buf.push('</table>');
         }
         else {
             columns.forEach(function (key) {
                 if (typeof obj[key] === 'object' && !Array.isArray(obj[key])) {
-                    buf.push('<td' + getClass(obj[key]) + '>', tableify(obj[key], false, parents), '</td>');
+                    buf.push('|', tableify(obj[key], false, parents));
                 }
                 else {
-                    buf.push('<td' + getClass(obj[key]) + '>', tableify(obj[key], columns, parents), '</td>');
+                    buf.push('|', tableify(obj[key], columns, parents));
                 }
             });
         }
@@ -93,18 +87,4 @@ function tableify(obj, columns, parents) {
     }
 
     return buf.join('');
-}
-
-function getClass(obj) {
-    return ' class="' 
-        + ((obj && obj.constructor)
-            ? obj.constructor.name 
-            : typeof obj
-        ).toLowerCase()
-        + ((obj === null)
-            ? ' null'
-            : ''
-        )
-        + '"'
-        ;
 }
